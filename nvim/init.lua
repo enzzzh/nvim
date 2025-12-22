f455dfacc1d1cb70a828a3de912ea874273cce08
Enzo <enzohakim2008@gmail.com>
2025-12-22 10:21:39 +0200

    Will automatically run lua ColorMyPencils()

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,4 +2,8 @@ require ("plugin.remap")
 require ("plugin.color")
 require ("plugin.packer")
 vim.opt.swapfile = false
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  command = "lua ColorMyPencils()",
+})
 
