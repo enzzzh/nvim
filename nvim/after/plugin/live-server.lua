8ba9c969d9b4a6a33c585434fc1cfd9231b7eaf8
Enzo <enzohakim2008@gmail.com>
2026-01-08 22:27:13 +0200

    Added live server and fixed some stuff

---
+++ nvim/after/plugin/live-server.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>ls", ":LiveServerStart<CR>")
+vim.keymap.set("n", "<leader>lS", ":LiveServerStop<CR>")
--- nvim/lua/plugin/packer.lua
+++ nvim/lua/plugin/packer.lua
@@ -1,42 +1,48 @@
 vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-	use{
-    		'nvim-telescope/telescope.nvim', tag = '0.1.x',
-    		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-
---	use({
---	  'rose-pine/nvim',
---	  as = 'rose-pine',
---	  config = function()
---	  vim.cmd('colorscheme rose-pine')	  
---	  end
---	})
-
-	use({
-		"bluz71/vim-moonfly-colors",
-		as = "moonfly",
-		config = function()
-		vim.cmd('colorscheme moonfly')
-		end
-	})
-
-	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use ('theprimeagen/harpoon')
-	use ('mbbill/undotree')
-	use 'neovim/nvim-lspconfig'   -- The LSP client configuration manager
-    	use 'williamboman/mason.nvim'         -- Language Server installer
-    	use 'williamboman/mason-lspconfig.nvim' -- Connects Mason to nvim-lspconfig
-	use 'akinsho/toggleterm.nvim'
-    	use 'hrsh7th/nvim-cmp'        
-	use 'hrsh7th/cmp-nvim-lsp'   
-	use 'L3MON4D3/LuaSnip'       
-	use {
-    	'windwp/nvim-autopairs', 
-    	config = function()
-        require('nvim-autopairs').setup({})
-    end
-}
-end)
+ 
+ return require('packer').startup(function(use)
+       use 'wbthomason/packer.nvim'
+         use {
+                 'nvim-telescope/telescope.nvim', tag = '0.1.x',
+                 requires = { {'nvim-lua/plenary.nvim'} }
+         }
+     
+    --    use({
+    --      'rose-pine/nvim',
+    --      as = 'rose-pine',
+    --      config = function()
+    --      vim.cmd('colorscheme rose-pine')      
+    --      end
+    --    })
+    
+        use {
+            'bluz71/vim-moonfly-colors',
+            as = 'moonfly',
+            config = function()
+            vim.cmd('colorscheme moonfly')
+            end
+        }
+    
+        use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+        use 'theprimeagen/harpoon'
+        use 'mbbill/undotree'
+        use 'neovim/nvim-lspconfig'   -- The LSP client configuration manager
+            use 'williamboman/mason.nvim'         -- Language Server installer
+           use 'williamboman/mason-lspconfig.nvim'
+        use 'akinsho/toggleterm.nvim'
+            use 'hrsh7th/nvim-cmp'        
+        use 'hrsh7th/cmp-nvim-lsp'   
+        use 'L3MON4D3/LuaSnip' 
+    
+          use {
+            'aurum77/live-server.nvim',
+            run = 'npm install -g live-server'
+          }
+    
+          use {
+            'windwp/nvim-autopairs', 
+            config = function()
+            require('nvim-autopairs').setup({})
+        end
+        }
+    end)
